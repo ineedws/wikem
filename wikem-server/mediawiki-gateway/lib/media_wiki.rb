50d5923375f64072f62cb59e8ae23b39c321be04
sbxmal <2webfl@gmail.com>
2013-09-19 02:09:04 +0300

    Revert "Initial commit"
    This reverts commit 6f3c80c320fafdbe0f7c08170aecee94b46008dc.

--- wikem-server/mediawiki-gateway/lib/media_wiki.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/media_wiki/config'
-require File.dirname(__FILE__) + '/media_wiki/utils'
-require File.dirname(__FILE__) + '/media_wiki/gateway'
-
-module MediaWiki
-  VERSION = "0.3.4"
-end
--- wikem-server/test_delete_2012.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'sqlite3'
-
-  db = SQLite::Database.new( "deletethistesetdb" )
-db.cache_size=3000
- # db.execute( "select * from table" ) do |row|
-   # p row
-  
-  db.close
